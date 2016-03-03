3f52643a3f05b6394a1ed236d309997712d03f1a
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2016-03-03 14:34:19 +0200

    Update generate_optimal_configs.sh

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+. /etc/jelastic/environment
+
 SED=$(which sed);
 
-#
-# config optimizer for neo4j
-#
+ORIENTDB_RUN_SCRIPT="${OPENSHIFT_ORIENTDB_DIR}/versions/${Version}/bin/server.sh";
 
-ORIENTDB_RUN_SCRIPT="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/bin/server.sh";
+if [ -e ${OPENSHIFT_ORIENTDB_DIR}/versions/${Version}/bin/variablesparser.sh ]; then
+      . ${OPENSHIFT_ORIENTDB_DIR}/versions/${Version}/bin/variablesparser.sh
+fi
 
- [ -z "$XMS" ] && { XMS=32m; }
- memory_total=`free -m | grep Mem | awk '{print $2}'`;
- [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}m";  }
+[ -z "$XMS" ] && { XMS=32m; }
+memory_total=`free -m | grep Mem | awk '{print $2}'`;
+[ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}m";  }
+let MAXDISKCACHE=memory_total-67;
 
 $SED -i "s/-Xmx\([0-9]*[mM]\)*/-Xmx${XMX}/" $ORIENTDB_RUN_SCRIPT;
 $SED -i "s/-Xms\([0-9]*[mM]\)*/-Xms${XMS}/" $ORIENTDB_RUN_SCRIPT;
-
+$SED -i "s/\"-Dstorage.diskCache.bufferSize=[0-9]*\"/\"-Dstorage.diskCache.bufferSize=$MAXDISKCACHE\"/" $ORIENTDB_RUN_SCRIPT;
