f1b29f786d15fbee9eb1934481bd5c2de107b222
SiryjVyiko <dz@hivext.net>
2016-03-04 11:03:17 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -2,21 +2,16 @@
 
 SED=$(which sed);
 
-#
-# This is an example of reset password hook in Jelastic
-#
+. /etc/jelastic/environment
 
-#$J_OPENSHIFT_APP_ADM_USER        ;   Operate this variable for the username
-#$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 J_OPENSHIFT_APP_ADM_USER="root";
 
 function _setPassword() {
-    return 0;
     ORIENTDB_SERVER_PROP_FILE="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/config/orientdb-server-config.xml";
-    service cartridge stop >> /dev/null 2>&1;
+    kill -9 $(ps aux|grep 'orientdb\|control start\|server.sh'|grep -v grep|awk '{print $2}') >> /dev/null 2>/dev/null;
     sed -i '/<user name=\"root\".*password=/d' $ORIENTDB_SERVER_PROP_FILE;
     sed -i '/<user name=\"guest\".*password=/d' $ORIENTDB_SERVER_PROP_FILE;
     sed -i "/<users>/a <user name=\"guest\" password=\"$J_OPENSHIFT_APP_ADM_PASSWORD\" resources=\"connect,server.listDatabases,server.dblist\"\/>" $ORIENTDB_SERVER_PROP_FILE;
     sed -i "/<users>/a <user name=\"root\" password=\"$J_OPENSHIFT_APP_ADM_PASSWORD\" resources=\"*\"\/>" $ORIENTDB_SERVER_PROP_FILE;
-    service cartridge start >> /dev/null 2>&1;
+    /etc/init.d/cartridge start >> /dev/null 2>&1;
 }
